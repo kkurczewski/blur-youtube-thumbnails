137591421b6e9788e1956b5bf0c702032865a689
kurczews <krzysiek.kurczewski@gmail.com>
2023-04-03 20:28:15 +0200

    Fix home page blur on Chrome
    On Chrome, contrary to Chromium, initial home page doesn't blur all videos.

--- src/content/selectors/home.js
+++ src/content/selectors/home.js
@@ -7,7 +7,6 @@ const home = (() => {
         title: "#details #video-title",
     }
     const videoSelector = `#content:has(ytd-thumbnail):has(${selectors.channel}):has(${selectors.title})`
-    console.log(videoSelector)
 
     return {
         path: "/",
@@ -15,7 +14,7 @@ const home = (() => {
         process: async (root, callback) => {
             const container = await find(root, "#contents.ytd-rich-grid-renderer")
             observe(container, "ytd-rich-grid-row", processRow)
-            processRow(container.querySelector("ytd-rich-grid-row"))
+            container.querySelectorAll("ytd-rich-grid-row").forEach(processRow)
 
             function processRow(row) {
                 row.querySelectorAll(videoSelector).forEach(callback)
