869745f41939050f4849a52c212a539138ad21d9
kurczews <krzysiek.kurczewski@gmail.com>
2023-05-13 10:53:32 +0200

    Add tags matcher in options

--- src/options/index.js
+++ src/options/index.js
@@ -1,53 +1,59 @@
-const nodes = (() => {
-  const channels = document.querySelector("#channels")
-  const keywords = document.querySelector("#keywords")
-  return {
-    channels: {
-      whitelist: channels.querySelector(".whitelist"),
-      blacklist: channels.querySelector(".blacklist"),
-    },
-    keywords: {
-      whitelist: keywords.querySelector(".whitelist"),
-      blacklist: keywords.querySelector(".blacklist"),
-    }
-  }
-})()
-
 window.onload = async () => {
-  const options = await preloadOptions()
+  loadTags()
 
-  assignEntries(nodes.channels.whitelist, options.channels.whitelist)
-  assignEntries(nodes.channels.blacklist, options.channels.blacklist)
-  assignEntries(nodes.keywords.whitelist, options.keywords.whitelist)
-  assignEntries(nodes.keywords.blacklist, options.keywords.blacklist)
+  document.querySelector("#save").onclick = saveTags
+  document.querySelector("#matcher input").oninput = matchTags
 
-  function assignEntries(list, entries) {
-    const firstElement = list.firstElementChild
-    entries.forEach(entry => {
-      const li = firstElement.cloneNode()
-      li.textContent = entry
-      list.append(li)
-    })
-    if (entries.length > 0) {
-      firstElement.remove()
+  async function loadTags() {
+    const options = await preloadOptions()
+    console.log(options)
+
+    for (let tagSection of document.querySelectorAll("fieldset:has(.tag)")) {
+      for (let tagList of tagSection.querySelectorAll(":has(> .tag)")) {
+        assignEntries(tagList, options[tagSection.id][tagList.className])
+      }
+    }
+
+    function assignEntries(list, entries) {
+      const firstElement = list.firstElementChild
+      entries.forEach(entry => {
+        const li = firstElement.cloneNode()
+        li.textContent = entry
+        list.append(li)
+      })
+      if (entries.length > 0) {
+        firstElement.remove()
+      }
     }
   }
-}
 
-document.querySelector("#save").onclick = () => {
-  const channels = {
-    whitelist: extractValues(nodes.channels.whitelist),
-    blacklist: extractValues(nodes.channels.blacklist),
-  }
-  const keywords = {
-    whitelist: extractValues(nodes.keywords.whitelist),
-    blacklist: extractValues(nodes.keywords.blacklist),
-  }
-  saveAllOptions({ channels, keywords })
+  async function saveTags() {
+    const options = {}
+    for (let tagSection of document.querySelectorAll("fieldset:has(.tag)")) {
+      options[tagSection.id] = {}
+      for (let tagList of tagSection.querySelectorAll(":has(> .tag)")) {
+        options[tagSection.id][tagList.className] = extractValues(tagList)
+      }
+    }
+    saveAllOptions(options)
 
-  function extractValues(list) {
-    return Array.from(list.children)
-      .map(child => child.innerText)
-      .filter(value => value)
+    function extractValues(list) {
+      return Array.from(list.children)
+        .map(child => child.innerText)
+        .filter(value => value)
+    }
+  }
+
+  async function matchTags(event) {
+    const input = event.srcElement.value
+    document
+      .querySelectorAll(".tag")
+      .forEach(node => {
+        const tagValue = node.innerText
+        if (tagValue.length > 0) {
+          const matches = input.match(RegExp(tagValue, "i")) != null
+          node.classList.toggle("highlight", matches)
+        }
+      })
   }
 }
