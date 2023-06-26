d54a716ac306d6497771a5b019cc8fea76532a7e
kurczews <krzysiek.kurczewski@gmail.com>
2023-06-26 17:48:56 +0200

    Make first installation more user friendly

--- src/hook/onInstall.js
+++ src/hook/onInstall.js
@@ -11,5 +11,6 @@ chrome.runtime.onInstalled.addListener(async () => {
   const options = await chrome.storage.local.get()
   if (Object.entries(options).length === 0) {
     chrome.storage.local.set({ channels, keywords })
+    chrome.runtime.openOptionsPage()
   }
 })
