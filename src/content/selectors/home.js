d76fc04c959cfaf0fb31bd79f0494be5a48919d4
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2023-08-20 09:43:35 +0200

    Fix last videos row on home page (Chrome)

--- src/content/selectors/home.js
+++ src/content/selectors/home.js
@@ -8,5 +8,11 @@ async function homePageObserver(root, videoCallback) {
     container
       .querySelectorAll(VIDEO_SELECTOR)
       .forEach(videoCallback)
+
+    // on Chrome last row may load with one or two videos instead full row of four
+    // what is peculiar is fact that problem doesn't occur on Chromium
+    if (container.matches("ytd-rich-grid-row:last-of-type")) {
+      observeDirectChildrens(container.querySelector("#contents"), videoCallback)
+    }
   })
 }
