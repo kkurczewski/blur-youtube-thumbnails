6b1a5f9736f9937d3cc1da659a90a4fd39b1eb92
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2023-09-16 15:51:31 +0200

    Fix watch-next page selectors

--- src/content/selectors/watch.js
+++ src/content/selectors/watch.js
@@ -2,9 +2,14 @@
 
 async function watchPageObserver(root, videoCallback) {
   const pageRoot = await find(root, "#page-manager > ytd-watch-flexy")
-  const scroll = await find(pageRoot, "#related #items")
+  const wrapper = await find(pageRoot, "#related #items")
+  const scroll = await find(wrapper, `:has(${VIDEO_CONTAINER}) #contents`)
 
   observeDirectChildrens(scroll, container => {
-    container.querySelectorAll(VIDEO_SELECTOR).forEach(videoCallback)
+    const video = container.querySelector(VIDEO_SELECTOR)
+    // last child is continuation element and it doesn't have video
+    if (video != null) {
+      videoCallback(video)
+    }
   })
 }
