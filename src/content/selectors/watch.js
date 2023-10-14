19a2c20cb3b55f9992dc03a1c8720a42e7c87fb6
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2023-10-14 15:16:04 +0200

    Fix: Youtube page structure apparently changed

--- src/content/selectors/watch.js
+++ src/content/selectors/watch.js
@@ -3,9 +3,8 @@
 async function watchPageObserver(root, videoCallback) {
   const pageRoot = await find(root, "#page-manager > ytd-watch-flexy")
   const wrapper = await find(pageRoot, "#related #items")
-  const scroll = await find(wrapper, `:has(${VIDEO_CONTAINER}) #contents`)
 
-  observeDirectChildrens(scroll, container => {
+  observeDirectChildrens(wrapper, container => {
     const video = container.querySelector(VIDEO_SELECTOR)
     // last child is continuation element and it doesn't have video
     if (video != null) {
