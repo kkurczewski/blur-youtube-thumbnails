47811741145ddde11d6a299f83dd0cfcadd65307
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2023-10-15 21:13:15 +0200

    Revert home selector changes to 1.3.1
    Recyling elements fails on home page. Seems that issue materializes when tab was idle for longer time.
    
    Probably monitoring every node in row causes some problem, to be confirmed.

--- src/content/selectors/home.js
+++ src/content/selectors/home.js
@@ -1,20 +1,24 @@
 // home page
 
+const VIDEO_CONTAINER = "#dismissible"
+const VIDEO_TITLE = "#video-title"
+const VIDEO_SELECTOR = `${VIDEO_CONTAINER}:has(${VIDEO_TITLE}):not(:has(${VIDEO_CONTAINER}))`
+
 async function homePageObserver(root, videoCallback) {
   const pageRoot = await find(root, "#page-manager > ytd-browse[page-subtype=home]")
   const scroll = await find(pageRoot, "#primary #contents")
 
-  const rowObserver = new DirectChildObserver(video => {
-    if (video.matches(":has(#video-title)")) {
-      videoCallback(video)
-    }
-  })
+  const tailObserver = new DirectChildObserver(videoCallback)
 
-  observeDirectChildrens(scroll, wrapper => {
-    const row = wrapper.querySelector("#contents:not(:has(#contents))")
-    // last element is continuation and always results in null
-    if (row != null) {
-      rowObserver.observe(row)
+  observeDirectChildrens(scroll, container => {
+    container
+      .querySelectorAll(VIDEO_SELECTOR)
+      .forEach(videoCallback)
+
+    // last loaded row may load with one or two videos instead full row of four elements
+    // after scrolling down row is filled up but that elements are missed in top observer
+    if (container.matches("ytd-rich-grid-row:last-of-type:not(:first-child)")) {
+      tailObserver.observe(container.querySelector("#contents"))
     }
   })
 }
