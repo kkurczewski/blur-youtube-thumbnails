5b36e6d6b29fff84cf17568d5dfb0d941c6f026f
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2023-10-15 12:56:09 +0200

    Fix nested videos in search results
    Instead checking #dismissible tag check if element has nested #items.
    
    Also, properly handle dynamic content added to  #items (show more, etc.)

--- src/content/selectors/results.js
+++ src/content/selectors/results.js
@@ -4,11 +4,12 @@ async function resultsPageObserver(root, videoCallback) {
   const pageRoot = await find(root, "#page-manager > ytd-search")
   const scroll = await find(pageRoot, "#primary #contents")
 
-  const wrapperObserver = new DirectChildObserver(container => {
-    // playlist are single node but not #dismissible
-    if (container.matches("ytd-playlist-renderer")) videoCallback(container)
-    // shelves and single videos are nested in #dismissible
-    else container.querySelectorAll(VIDEO_SELECTOR).forEach(videoCallback)
+  const wrapperObserver = new DirectChildObserver(element => {
+    if (element.matches(":has(#items #video-title")) {
+      wrapperObserver.observe(element.querySelector("#items"))
+    } else if (element.matches(":has(#video-title)")) { // channel renderer has no title
+      videoCallback(element)
+    }
   })
 
   observeDirectChildrens(scroll, async page => {
